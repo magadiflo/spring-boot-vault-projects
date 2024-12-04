933b6cf383084dc26426838dff5c175e34cb651c
Martín <magadiflo@gmail.com>
2024-12-04 11:37:50 -0500

    modifica paquete principal de spring-boot-vault-approle

--- spring-boot-vault-approle/src/main/java/dev/magadiflo/app/SpringBootVaultApproleApplication.java
+++ spring-boot-vault-approle/src/main/java/dev/magadiflo/app/SpringBootVaultApproleApplication.java
@@ -1,4 +1,4 @@
-package dev.magadiflo.spring_boot_vault_approle;
+package dev.magadiflo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-vault-approle/src/test/java/dev/magadiflo/app/SpringBootVaultApproleApplicationTests.java
+++ spring-boot-vault-approle/src/test/java/dev/magadiflo/app/SpringBootVaultApproleApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.magadiflo.spring_boot_vault_approle;
+package dev.magadiflo.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
